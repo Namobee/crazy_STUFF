ac4a0dbb1a7619234d4338bb23cd56278ae01e1b
Namobee <109030386+Namobee@users.noreply.github.com>
2021-12-10 05:42:41 +0000

    updated appveyor config

--- src/App.tsx
+++ src/App.tsx
@@ -2,3 +2,6 @@
 import React from 'react'
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { GlobalStyle } from 'ui'
+import { SIGNIN } from './routes'
+import { SignIn } from './pages'
