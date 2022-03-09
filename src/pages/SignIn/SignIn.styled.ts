2cfa11ef35d706d4785cff9444ff9269e3241b84
Namobee <109030386+Namobee@users.noreply.github.com>
2022-03-09 09:06:08 +0000

    izettle-toolbox:\u00a0prepare for next development iteration

--- src/pages/SignIn/SignIn.styled.ts
+++ src/pages/SignIn/SignIn.styled.ts
@@ -2,4 +2,4 @@ import styled from 'styled-components/macro'
 import { theme } from 'config'
 
 export const Test = styled.h1`
-  color: ${theme.colors.neutral[9
+  color: ${theme.colors.neutral[900]
