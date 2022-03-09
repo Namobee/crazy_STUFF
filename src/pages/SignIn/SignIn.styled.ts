33975f32f8f3640e1971759fbf7476d00ea17008
Namobee <109030386+Namobee@users.noreply.github.com>
2022-03-09 10:16:12 +0000

    Updated to last kernel.

--- src/pages/SignIn/SignIn.styled.ts
+++ src/pages/SignIn/SignIn.styled.ts
@@ -2,4 +2,5 @@ import styled from 'styled-components/macro'
 import { theme } from 'config'
 
 export const Test = styled.h1`
-  color: ${theme.colors.neutral[900]
+  color: ${theme.colors.neutral[900]};
+`
