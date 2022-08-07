20a92edf65050ce20151d492c639202b560f9c97
Namobee <109030386+Namobee@users.noreply.github.com>
2022-08-07 22:14:51 +0000

    life

--- src/ui/components/GlobalStyle.ts
+++ src/ui/components/GlobalStyle.ts
@@ -11,4 +11,5 @@ export const GlobalStyle = createGlobalStyle`
 
   body {
     background: ${theme.colors.neutral[200]};
-    font-family: ${theme.font.family
+    font-family: ${theme.font.family};
+   
