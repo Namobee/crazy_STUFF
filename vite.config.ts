32c468a3f4d4732d4f0844f1b4bb8b82a4564d78
Namobee <109030386+Namobee@users.noreply.github.com>
2023-03-04 21:41:31 +0000

    Delete License

--- vite.config.ts
+++ vite.config.ts
@@ -4,3 +4,6 @@ import reactRefresh from '@vitejs/plugin-react-refresh'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    reactRefresh(),
